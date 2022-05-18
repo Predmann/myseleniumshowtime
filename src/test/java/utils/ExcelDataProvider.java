105cbccd1165f7e9d01dbb69531fbed65ddda625
Predmann <przemyslaw.redmann@gmail.com>
2022-05-18 22:51:27 +0200

    Added excelDataProvider class and testNG dependency

--- src/test/java/utils/ExcelDataProvider.java
+++ src/test/java/utils/ExcelDataProvider.java
@@ -1,26 +1,43 @@
 package utils;
 
-public class ExcelDataProvider {
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-	public static void main(String[] args) {
+public class ExcelDataProvider {
+	
+	@Test(dataProvider="test1data")
+	public void test1(String username, String password) {
+		System.out.println(username+ " | "+password);
+	}
+
+	@DataProvider(name = "test1data")
+	public Object[][] getData() {
 		String excelPath = "D:\\workspace\\myseleniumshowtime\\excel\\data.xlsx";
-		testData(excelPath, "Sheet1");
+		
+				Object data[][] = testData(excelPath, "Sheet1");
+		return data;
 		
 	}
-	public static void testData(String excelPath, String sheetName) {
-		
+
+	public static Object[][] testData(String excelPath, String sheetName) {
+
 		ExcelUtils excel = new ExcelUtils(excelPath, sheetName);
-		
+
 		int rowCount = excel.getRowCount();
 		int colCount = excel.getColCount();
-		
-		for (int i=1; i<rowCount; i++) {
-			for(int j=0; j<colCount; j++) {
-				
+
+		Object data[][] = new Object[rowCount - 1][colCount];
+
+		for (int i = 1; i < rowCount; i++) {
+			for (int j = 0; j < colCount; j++) {
+
 				String cellData = excel.getCellDataString(i, j);
-				System.out.print(cellData+" | ");
+				System.out.print(cellData + " | ");
+				data[i - 1][j] = cellData;
+
 			}
 			System.out.println();
 		}
+		return data;
 	}
 }
